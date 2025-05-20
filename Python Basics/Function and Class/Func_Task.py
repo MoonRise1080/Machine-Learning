533b4da5c7465b6c7fc636b234a0c874306cf5c9
MoonRise <152148710+MoonRise1080@users.noreply.github.com>
2025-05-20 22:37:38 +0600

    Function task

---
+++ Python Basics/Function and Class/Func_Task.py
@@ -0,0 +1,9 @@
+def NID(name, dob, gender, preAdd, perAdd):
+    print(f"Name: {name}")
+    print(f"Date of Birth: {dob}")
+    print(f"Gender: {gender}")
+    print(f"Present Address: {preAdd}")
+    print(f"Permanent Address: {perAdd}")
+
+
+NID("Nehal", "11.6.2003", "Male", "Grove Street, San Andreas", "Grove Street, San Andreas")
