6dd606fe8e4011f176cdac49f8153a52860b6a22
MoonRise <152148710+MoonRise1080@users.noreply.github.com>
2025-05-20 14:58:00 +0600

    Loop tasks

---
+++ Python Basics/Loops/Task_1.py
@@ -0,0 +1,23 @@
+"""
+task 1 : (Using while loop)
+Output: 
+1 2 3
+4 5 6
+7 8 9
+
+"""
+
+i = 1
+k = 1
+
+while i <= 3:
+    j = 1
+    while j <= 3:
+        print(k, end = "")
+        j += 1
+        k += 1
+    i += 1
+    print("")
+    
+
+    
